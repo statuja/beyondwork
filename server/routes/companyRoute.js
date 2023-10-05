6c47b677b6f0ba6385ec69b109b413c7f8c2ec66
dci-student <irinasolovich@gmail.com>
2023-10-05 15:03:03 +0200

    validation created

--- server/routes/companyRoute.js
+++ server/routes/companyRoute.js
@@ -1,7 +1,35 @@
 import express from "express";
+import Company from "../models/Company.js";
+import { check, validationResult } from "express-validator";
 import { createCompany } from "../controllers/companyControllers.js";
 
 const router = express.Router();
-router.post("/create", createCompany);
+
+const validation = [
+  check("companyContact.email")
+    .notEmpty()
+    .isEmail()
+    .withMessage("email is not valid")
+    .custom(async (value) => {
+      const company = await Company.findOne({ "companyContact.email": value });
+      if (company) {
+        throw new Error("E-mail already in use");
+      }
+    })
+    .withMessage("email is already in use"),
+];
+
+router.post(
+  "/create",
+  validation,
+  (req, res) => {
+    const error = validationResult(req);
+    if (error.isEmpty()) {
+      return res.sendStatus(201);
+    }
+    res.send({ error: error.array() });
+  },
+  createCompany
+);
 
 export default router;
