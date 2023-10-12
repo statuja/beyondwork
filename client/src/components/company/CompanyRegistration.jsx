a44c81473f533a6c23b5256632f99dccaa711eb2
dci-student <irinasolovich@gmail.com>
2023-10-12 11:04:32 +0200

    Registration modified

--- client/src/components/company/CompanyRegistration.jsx
+++ client/src/components/company/CompanyRegistration.jsx
@@ -1,15 +1,18 @@
 import React, { useState } from "react";
-import { useForm } from 'react-hook-form';
-//import dotenv from "dotenv";
+import { useForm } from "react-hook-form";
 
 export const CompanyRegistration = () => {
-
-  const { register, handleSubmit, reset, formState: { errors } } = useForm();
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
 
   const [message, setMessage] = useState("");
   const [error, setError] = useState("");
 
-  const onSubmit = data => {
+  const onSubmit = (data) => {
     const newData = {
       companyName: data.companyName,
       companyType: data.companyType,
@@ -25,15 +28,19 @@ export const CompanyRegistration = () => {
         phoneNumber: data.phoneNumber,
       },
       defaultAdminEmail: data.defaultAdminEmail,
-    }
-    console.log(newData)
-    fetch("http://localhost:5000/company/create", {
-      method: "POST",
-      body: JSON.stringify(newData),
-      headers: {
-        "Content-Type": "application/json",
+    };
+    //console.log(newData);
+    fetch(
+      "http://localhost:5000/company/create",
+      {
+        method: "POST",
+        body: JSON.stringify(newData),
+        headers: {
+          "Content-Type": "application/json",
+        },
       },
-    })
+      reset()
+    )
       .then((res) => {
         return res.json();
       })
@@ -41,7 +48,6 @@ export const CompanyRegistration = () => {
         //console.log(data);
         if (data.error) {
           setError(data.error[1].msg);
-          
         } else {
           setMessage(`You successfully register your company. here is your Admin email ${data.defaultAdminEmail} and your temporary password: admin1234
         please change your logging password and update your details.`);
@@ -49,40 +55,61 @@ export const CompanyRegistration = () => {
       })
       .catch((err) => {
         setError(err.msg);
-       
       });
   };
   console.log(errors);
- 
-      return (
-        <div>
-          <h1>Register your company </h1>
-       
-          <form onSubmit={handleSubmit(onSubmit)}>
-          <input type="text" placeholder="Company Name" {...register("companyName", {required: true})} />
-          <input type="text" placeholder="Company Type " {...register("companyType", {required: true})} />
-          <select {...register("numberOfEmployees")}>
-            <option value="<50">0-50</option>
-            <option value="51-100">51-100</option>
-            <option value="101-500">101-500</option>
-            <option value=">500">more than 500</option>
-          </select>
-          <input type="text" placeholder="Street, Building, Office(if applicable) No" {...register("address", {required: true})} />
-          <input type="text" placeholder="ZipCode" {...register("zipCode", {required: true})} />
-          <input type="text" placeholder="City" {...register("city")}/>
-          <input type="text" placeholder="Country" {...register("country")} />
-          <input type="text" placeholder="Phone Number" {...register("phoneNumber")} />
-          <input type="email" placeholder="Company E-mail Address" {...register("email" )} />
-          <input type="email" placeholder="Admin E-mail Address" {...register("defaultAdminEmail" )} />
-    
-          <input type="submit" />
-         
-            {error && <div>Error: {error}</div>} {message && <div>{message}</div>}
-            {/* Display error message if there is an error */}
-        </form>
-        </div>
-      );
-  };
 
-  // console.log(error);
- 
+  return (
+    <div>
+      <h1>Register your company </h1>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          type="text"
+          placeholder="Company Name"
+          {...register("companyName", { required: true })}
+        />
+        <input
+          type="text"
+          placeholder="Company Type "
+          {...register("companyType", { required: true })}
+        />
+        <select {...register("numberOfEmployees")}>
+          <option value="<50">0-50</option>
+          <option value="51-100">51-100</option>
+          <option value="101-500">101-500</option>
+          <option value=">500">more than 500</option>
+        </select>
+        <input
+          type="text"
+          placeholder="Street, Building, Office(if applicable) No"
+          {...register("address", { required: true })}
+        />
+        <input
+          type="text"
+          placeholder="ZipCode"
+          {...register("zipCode", { required: true })}
+        />
+        <input type="text" placeholder="City" {...register("city")} />
+        <input type="text" placeholder="Country" {...register("country")} />
+        <input
+          type="text"
+          placeholder="Phone Number"
+          {...register("phoneNumber")}
+        />
+        <input
+          type="email"
+          placeholder="Company E-mail Address"
+          {...register("email")}
+        />
+        <input
+          type="email"
+          placeholder="Admin E-mail Address"
+          {...register("defaultAdminEmail")}
+        />
+        <input type="submit" />
+        {error && <div>Error: {error}</div>} {message && <div>{message}</div>}
+      </form>
+    </div>
+  );
+};
