787e19487338c9f184f4da16b9c9001176809e5d
Marwahaldujaili <aldujailimarwa@gmail.com>
2023-10-12 15:50:59 +0200

    nav and footer created

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,18 @@
 import "./App.css";
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { CompanyRegistration } from "./components/CompanyRegistration/CompanyRegistration";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <Routes>
         <Route path="/" element={<CompanyRegistration />} />
       </Routes>
+      <Footer/>
     </div>
   );
 }
---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Navbar.scss";
+import { Link } from "react-router-dom";
+import logo from "../../images/Logo_green.png";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="left">
+        <Link to="about">About us</Link>
+      </div>
+      <div className="center">
+        <Link to="/">
+          <img src={logo} alt="BeyondWork Logo" />
+        </Link>
+      </div>
+      <div className="right">
+        <Link to="/contact">Contact</Link>
+      </div>
+    </div>
+  );
+}
+export default Navbar;
