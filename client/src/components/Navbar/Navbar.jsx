fc6c207ce3a19e9aa3ba2e236879da858020a10e
Marwahaldujaili <aldujailimarwa@gmail.com>
2023-10-18 11:01:05 +0200

    final company reg

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,25 +1,20 @@
 import React from "react";
 import "./Navbar.scss";
 import { Link } from "react-router-dom";
-<<<<<<< HEAD
-=======
-import icon from "../../images/small_icon_green.png"
+import icon from "../../images/small_icon_green.png";
 //import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 //import {faFacebookF} from "@fortawesome/free-brands-svg-icons";
->>>>>>> 0620b54e31d3f8b226aebb85f881dfb746afe6b2
 
 function Navbar() {
   return (
     <div className="navbar">
       <div className="left">
-      <Link to="/user/login">
-        <img src={icon} alt="small-icon"/>
-      </Link>
-       
-
+        <Link to="/user/login">
+          <img src={icon} alt="small-icon" />
+        </Link>
       </div>
       <div className="right">
-      <Link to="/about">About us</Link>
+        <Link to="/about">About us</Link>
         <Link to="/contact">Contact</Link>
       </div>
     </div>
