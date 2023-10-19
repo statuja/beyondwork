05a3bdc32b84a5d233e0c95855ca5f24144049ad
netta8 <netta7398@gmail.com>
2023-10-19 15:41:08 +0200

    newsfeed all posts

--- client/src/context/MyProvider.jsx
+++ client/src/context/MyProvider.jsx
@@ -1,15 +1,12 @@
-import React from "react";
 import { useState, useEffect } from "react";
-import axios from "axios";
 import MyContext from "./MyContext";
 
 const MyProvider = ({ children }) => {
-  // const [userCompany, setUserCompany] = useState("");
   const [adminEmail, setAdminEmail] = useState("your admin email");
   const [companyName, setCompanyName] = useState("your company");
   const [companyData, setCompanyData] = useState(null);
   const [userData, setUserData] = useState(null);
-  // const [userId, setUserId] = useState("");
+  const [posts, setPosts] = useState([]);
 
   const updateCompanyData = (newData) => {
     setCompanyData(newData);
@@ -33,9 +30,23 @@ const MyProvider = ({ children }) => {
     getUser();
   }, []);
 
-  // const handlePostCreated = (newPost) => {
-  //   setPosts([newPost, ...posts]);
-  // };
+  useEffect(() => {
+    const getallPosts = async () => {
+      try {
+        const response = await fetch("http://localhost:5000/post/all", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          credentials: "include",
+        });
+        setPosts(response);
+      } catch (error) {
+        setPosts(null);
+      }
+    };
+    getallPosts();
+  }, []);
 
   return (
     <MyContext.Provider
