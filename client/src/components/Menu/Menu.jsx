a66ec5ea2334b4bdde03843980be3c68842d1e49
netta8 <netta7398@gmail.com>
2023-10-26 15:21:07 +0200

    your colleagues style

--- client/src/components/Menu/Menu.jsx
+++ client/src/components/Menu/Menu.jsx
@@ -3,7 +3,7 @@ import { Link, useNavigate } from "react-router-dom";
 import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
 import ApartmentOutlinedIcon from "@mui/icons-material/ApartmentOutlined";
 //import FeedIcon from '@mui/icons-material/Feed';
-import NewspaperIcon from '@mui/icons-material/Newspaper';
+import NewspaperIcon from "@mui/icons-material/Newspaper";
 import PersonAddAltOutlinedIcon from "@mui/icons-material/PersonAddAltOutlined";
 import BookmarkBorderOutlinedIcon from "@mui/icons-material/BookmarkBorderOutlined";
 import Groups2OutlinedIcon from "@mui/icons-material/Groups2Outlined";
@@ -41,60 +41,58 @@ const Menu = () => {
     }
   };
   return (
-   
-      <ul className="menu">
-        <li>
-          <Link to="/user/profile/me">
-            {" "}
-            <AccountCircleOutlinedIcon className="icon" /> My Profile
-          </Link>
-        </li>
-        <li>
-          <Link to="/company/profile">
-            <ApartmentOutlinedIcon className="icon" /> Company Profile
-          </Link>
-        </li>
-        <li>
-          <Link to="/company/profile">
-            <NewspaperIcon className="icon" /> News Feed
-          </Link>
-        </li>
-        <li>
-          <Link to="/user/create">
-            <PersonAddAltOutlinedIcon className="icon" /> Add New User
-          </Link>
-        </li>
-        <li>
-          <Link to="/savedposts">
-            <BookmarkBorderOutlinedIcon className="icon" /> Saved Posts
-          </Link>
-        </li>
-        <li>
-          <Link to="/allusers">
-            <Groups2OutlinedIcon className="icon" /> Your Team
-          </Link>
-        </li>
-        <li>
-          <Link to="">
-            <CalendarMonthIcon className="icon" /> Events
-          </Link>
-        </li>
-        <li>
-          <Link to="">
-            <StorefrontIcon className="icon" /> Marketplace
-          </Link>
-        </li>
-        <li>
-          <Link to="">
-            <TipsAndUpdatesOutlinedIcon className="icon" /> Suggestions Box
-          </Link>
-        </li>
-        
-        <li onClick={handleOnClick}>
-          <LogoutOutlinedIcon className="icon" /> Logout
-        </li>
-      </ul>
-   
+    <ul className="menu">
+      <li>
+        <Link to="/user/profile/me">
+          {" "}
+          <AccountCircleOutlinedIcon className="icon" /> My Profile
+        </Link>
+      </li>
+      <li>
+        <Link to="/company/profile">
+          <ApartmentOutlinedIcon className="icon" /> Company Profile
+        </Link>
+      </li>
+      <li>
+        <Link to="/company/profile">
+          <NewspaperIcon className="icon" /> News Feed
+        </Link>
+      </li>
+      <li>
+        <Link to="/user/create">
+          <PersonAddAltOutlinedIcon className="icon" /> Add New User
+        </Link>
+      </li>
+      <li>
+        <Link to="/savedposts">
+          <BookmarkBorderOutlinedIcon className="icon" /> Saved Posts
+        </Link>
+      </li>
+      <li>
+        <Link to="/allusers">
+          <Groups2OutlinedIcon className="icon" /> Your Colleagues
+        </Link>
+      </li>
+      <li>
+        <Link to="">
+          <CalendarMonthIcon className="icon" /> Events
+        </Link>
+      </li>
+      <li>
+        <Link to="">
+          <StorefrontIcon className="icon" /> Marketplace
+        </Link>
+      </li>
+      <li>
+        <Link to="">
+          <TipsAndUpdatesOutlinedIcon className="icon" /> Suggestions Box
+        </Link>
+      </li>
+
+      <li onClick={handleOnClick}>
+        <LogoutOutlinedIcon className="icon" /> Logout
+      </li>
+    </ul>
   );
 };
 
